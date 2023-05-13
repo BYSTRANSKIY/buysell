af800a3b116ed49e7ad7e63eae1e792c5644ebca
Misha Bystranskyi <mbystranskiy@gmail.com>
2023-05-13 13:45:28 +0300

    Uploading the photo to the database

--- src/main/java/com/example/buysell/controllers/ProductController.java
+++ src/main/java/com/example/buysell/controllers/ProductController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -27,13 +29,18 @@ public class ProductController {
 
     @GetMapping("product/{id}")
     public String productInfo(@PathVariable Long id, Model model) {
-        model.addAttribute("product", productService.getProduct(id));
+        Product product = productService.getProduct(id);
+        model.addAttribute("product", product);
+        model.addAttribute("images", product.getImages());
         return "product-info";
     }
 
     @PostMapping("/product/add")
-    public String addProduct(Product product) {
-        productService.saveProduct(product);
+    public String addProduct(Product product,
+                             @RequestParam("file1")MultipartFile file1,
+                             @RequestParam("file2")MultipartFile file2,
+                             @RequestParam("file3")MultipartFile file3) throws IOException {
+        productService.saveProduct(product, file1, file2, file3);
         return "redirect:/";
     }
 
--- src/main/java/com/example/buysell/services/ProductService.java
+++ src/main/java/com/example/buysell/services/ProductService.java
@@ -1,13 +1,15 @@
 package com.example.buysell.services;
 
 import com.example.buysell.models.Product;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface ProductService {
     List<Product> getProducts(String title);
     Product getProduct(Long id);
-    void saveProduct(Product product);
+    void saveProduct(Product product, MultipartFile file1, MultipartFile file2, MultipartFile file3) throws IOException;
     void deleteProduct(Long id);
 
 }
